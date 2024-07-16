f40dfa112adf39c9bb78ad6a5dfdb48bdd665ed1
Abubaker Alsiddiq  Ahmed <159482520+AbubakerAlsiddiq@users.noreply.github.com>
2024-07-16 10:31:10 -0400

    Updated Navbar.tsx
    Updated features:
    
    New Search bar Icon that toggles in and out from the rigtht side of the screen no matter the size of the screen.
    
    New Icon for users to create an account and save favorite items 
    
    New Icon shoppingcart Icon to place favorite items in 
    
    Updated toggle functionality to display menu for smaller screen sizes 
    
    cleaner look, moved links to socials to a new footer.tsx and added delmarva mediterrean market as a title.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,52 +1,27 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import Logo from "../assets/M&S New Logo.jpeg";
-import Instagram from "../assets/Instagram-Icon.png";
-import Facebook from "../assets/facebook-icon.png";
+import Logo from "../assets/M_S_new_logo.png";
 import "./Navbar.css";
-import SearchBar from "./SearchBars";
-import About from "./About";
-import Inventory from "./Inventory";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faSearch,
+  faShoppingBag,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-  const categories = [
-    "Balkan Valley",
-    "Beans",
-    "Biscuits/Cookies",
-    "Bread",
-    "Cheese",
-    "Choclates/Candies",
-    "Coffee",
-    "Dates",
-    "Drinks",
-    "Freezer #1",
-    "Frozen Vegetables",
-    "Grape Leaves",
-    "Honey",
-    "Jared Foods",
-    "Jelly/Jam",
-    "Natural Soaps",
-    "Meats",
-    "M&S Inhouse",
-    "Lentils",
-    "Mediterranean Sweets and Spreads",
-    "Nuts/Seeds",
-    "Oils/Butter/Ghee",
-    "Olives",
-    "Perfume/incense",
-    "Pickled Vegetables",
-    "Tobacco",
-    "Tea",
-    "Tahini",
-    "Spices",
-    "Rice",
-    "Yogurt/Yogurt Drinks",
-    "Wheat/Grain",
-  ];
-
+  const [showSearchForm, setShowSearchForm] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+
+  const handleSearchClick = useCallback(() => {
+    setShowSearchForm((prev) => !prev);
+  }, []);
+
+  const handleMenuToggle = useCallback(() => {
+    setIsMenuOpen((prev) => !prev);
+  }, []);
 
   useEffect(() => {
     setTimeout(() => {
@@ -55,58 +30,62 @@ const Navbar = () => {
   }, []);
 
   return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            <img src={Logo} alt="Logo" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a
-                  href="https://www.facebook.com/DelmarvaMediterraneanMarket/"
-                  target="_blank"
-                  rel="noopener"
-                  className="social-media-links"
-                >
-                  <img alt="Facebook" src={Facebook} />
-                </a>
-                <a
-                  href="https://www.instagram.com/delmarvamediterraneanmarket/"
-                  target="_blank"
-                  rel="noopener"
-                  className="social-media-links"
-                >
-                  <img alt="Instagram" src={Instagram} />
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active green-link" aria-current="page">
-                  <NavLink to="/about">About us</NavLink>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active green-link" aria-current="page">
-                  <NavLink to="/Inventory">Inventory</NavLink>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <img src={Logo} alt="Logo" className="navbar-logo" />
+        <NavLink to="/" className="navbar-brand">
+          <span>Delmarva</span>
+          <span>Mediterranean</span>
+          <span>Market</span>
+        </NavLink>
+      </div>
+      <div className={`navbar-menu ${isMenuOpen ? "open" : "close"}`}>
+        <NavLink to="/" className="nav-link">
+          Home
+        </NavLink>
+        <NavLink to="/Inventory" className="nav-link">
+          Inventory
+        </NavLink>
+        <NavLink to="/" className="nav-link">
+          Reviews
+        </NavLink>
+        <NavLink to="/about" className="nav-link">
+          About us
+        </NavLink>
+      </div>
+      <div className="navbar-icons">
+        <button
+          className="menu-btn"
+          onClick={handleMenuToggle}
+          aria-label="Toggle menu"
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <button
+          className="search-btn"
+          onClick={handleSearchClick}
+          aria-label="Search"
+        >
+          <FontAwesomeIcon icon={faSearch} />
+        </button>
+        <button className="cart-btn" aria-label="Shopping cart">
+          <FontAwesomeIcon icon={faShoppingBag} />
+        </button>
+        <button className="user-btn" aria-label="User profile">
+          <FontAwesomeIcon icon={faUser} />
+        </button>
+      </div>
+      <div className={`search-form ${showSearchForm ? "show" : "hide"}`}>
+        <input
+          type="search"
+          id="search-box"
+          placeholder="Search Inventory..."
+        />
+        <label htmlFor="search-box">
+          <FontAwesomeIcon icon={faSearch} />
+        </label>
+      </div>
+    </nav>
   );
 };
 
